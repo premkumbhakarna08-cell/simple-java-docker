cdf907feef87b60ac3d5375285b069e9632211ae
premkumbhakarna08-cell <premkumbhakarna08@gmail.com>
2025-12-17 11:19:53 +0000

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+        System.out.println("Hello, Docker! My Name is Prem and It's my first project , Current date: " + currentDate);
     }
 }
